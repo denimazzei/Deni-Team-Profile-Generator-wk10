da1482fdf102f612a128de6ee71b0de40c4dc957
Deni Mazzei <dconkell@gmail.com>
2021-09-30 20:18:25 -0500

    starter files for modularization

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//generate HTML file
+
+//initialize application
+//enter managers name, employee ID, email address, and office number
+    //will need library with team manager options
+//presented with menu to add: engineer or intern
+//select engineer option, , enter engineer name, ID, and github
+    //will need library for engineer options
+//select intern option, enter intern name, ID, email, school
+    //will need library for intern options
+
+    //will need a general employee library class for other fields to pull from
+    //index.js will need to require all 4 of these classes
+
+//taken back to menu after selection
+//when finished, HTML is generated
